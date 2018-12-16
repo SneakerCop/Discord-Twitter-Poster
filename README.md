8ec92ddde71836ad12bde1caf27be5f35f137a1a
notmonkee <44518957+notmonkee@users.noreply.github.com>
2018-12-16 13:41:52 -0500

    Initial Release

---
+++ README.md
@@ -0,0 +1,29 @@
+
+
+Prerequisites:
+
+A Linux/Windows/OSX server that's always running. (I host this program on a free google cloud debian server)
+
+A Discord bot user (Create one at https://discordapp.com/developers/applications/)
+
+A Twitter developer account (Apply for one at https://developer.twitter.com/en/apply-for-access.html)
+
+Install node.js (Download at https://nodejs.org/en/)
+
+Install these modules:
+
+request-promise-native
+discord.js
+twit
+
+Running:
+
+Replacre the TYPEHERE fields in settings.json with all your info.
+
+Invite the bot to your server using the following link (replace BOTCLIENTIDHERE with your client id in general info): 
+https://discordapp.com/oauth2/authorize?client_id=BOTCLIENTIDHERE&scope=bot&permissions=0.
+
+Run index.js
+(Optionally run with something like PM2)
+
+You're good to!
---
+++ index.js
@@ -0,0 +1,64 @@
+
+var Twitter = require('twit')
+var Discord = require("discord.js")
+var Request = require("request-promise-native")
+
+var settings = require("./settings.json")
+
+var client = new Twitter(settings.Twitter)
+
+var bot = new Discord.Client()
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+})
+
+bot.on("message", message => {
+    if (message.channel.id === settings.Discord.channel_id) {
+        message.attachments.forEach(Image => {
+            var url = Image.url
+
+            var promise = Request({
+                uri: Image.proxyURL,
+                encoding: null
+            }).then(imageData => {
+
+                client.post('media/upload', {
+                    media: imageData.toString("base64")
+                }, function(error, data, response) {
+                    if (!error) {
+                        var mediaIdStr = data.media_id_string
+                        var meta_params = {
+                            media_id: mediaIdStr,
+                            alt_text: {
+                                text: "Discord Image"
+                            }
+                        }
+
+                        client.post("media/metadata/create", meta_params, function(err, data, response) {
+                            if (!err) {
+                                var params = {
+                                    status: `New picture by ${message.author.username} ID ${Math.floor((Math.ran$
+                                    media_ids: [mediaIdStr]
+                                }
+                                client.post("statuses/update", params, function(err, data, response) {
+                                    if (!err) {
+                                        console.log(data)
+                                    } else {
+                                        throw err
+                                    }
+                                })
+                            } else {
+                                throw err
+                            }
+                        })
+                    } else {
+                        throw error
+                    }
+                })
+            })
+        })
+    }
+})
+
+bot.login(settings.Discord.token)
